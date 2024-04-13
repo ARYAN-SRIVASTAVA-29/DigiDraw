26da5acb220dd6425515b44243f9866569f8203e
Aryan srivastava <mainaryansrivastav@gmail.com>
2024-04-13 21:07:10 +0530

    established communication between menu and toolbox

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux'
 import styles from './index.module.css'
-import { COLORS } from '@/constants'
+import { COLORS, MENU_ITEMS } from '@/constants'
+
 
 const ToolBox = () => {
+
+    const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
+    const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL
+    const showBrushToolOption = activeMenuItem === MENU_ITEMS.PENCIL || MENU_ITEMS.ERASER
 
     const updateBrushSize = (e) => {
 
@@ -9,7 +15,7 @@ const ToolBox = () => {
     
     return (
         <div className={styles.toolboxContainer}>
-            <div className={styles.toolItem}>
+            {showStrokeToolOption &&  <div className={styles.toolItem}>
                 <h4 className={styles.toolText}>Stroke Color</h4>
                 <div className={styles.itemContainer}>
                     <div className={styles.colorBox} style={{backgroundColor: COLORS.BLACK}}/>
@@ -19,14 +25,15 @@ const ToolBox = () => {
                     <div className={styles.colorBox} style={{backgroundColor: COLORS.ORANGE}}/>
                     <div className={styles.colorBox} style={{backgroundColor: COLORS.YELLOW}}/>
                 </div>
-            </div>
+            </div>}
 
-            <div className={styles.toolItem}>
-                <h4 className={styles.toolText}>Brush Size</h4>
+            {showBrushToolOption && <div className={styles.toolItem}>
+                <h4 className={styles.toolText}>Brush Size {activeMenuItem}</h4>
                 <div className={styles.itemContainer}>
                     <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} />
                 </div>
-            </div>
+            </div>}
+            
         </div>
     )
 }
